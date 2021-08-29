8def2e80754f64fb72ebf422e04c57d80e93ba6d
arseniiarsenii <arseniivelichko2@gmail.com>
2021-08-30 02:30:50 +0300

    added new attribute

--- src/mastodon_meter/Account.py
+++ src/mastodon_meter/Account.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 from uuid import uuid4
 
 
@@ -10,3 +11,4 @@ class Account:
     instance: str
     id: str
     internal_id: str = field(default_factory=lambda: uuid4().hex)
+    added_on: datetime = field(default_factory=lambda: datetime.utcnow())
