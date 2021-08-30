392b6210c14b920ca7d65fc4ace564cc449fffbf
arseniiarsenii <arseniivelichko2@gmail.com>
2021-08-30 16:51:51 +0300

    finished MongoDB wrapper

--- src/mastodon_meter/database.py
+++ src/mastodon_meter/database.py
@@ -1,24 +1,26 @@
 import os
 import typing as tp
 from abc import ABC, abstractmethod
+from dataclasses import asdict
 
+from loguru import logger
 from pymongo import MongoClient
 
 from .Account import Account
 from .Metering import Metering
-from .Types import Collection
+from .Types import Collection, Document
 
 
 class DatabaseWrapper(ABC):
     """a common database wrapper interface"""
 
     @abstractmethod
-    def add_tracked_account(self, account_id: str, instance_url: str) -> None:
+    def add_tracked_account(self, account: Account) -> None:
         """add the provided account into the list of tracked accounts"""
         raise NotImplementedError
 
     @abstractmethod
-    def delete_tracked_account(self, account_id: str, instance_url: str) -> None:
+    def delete_tracked_account(self, account_internal_id: str) -> None:
         """delete the provided account from the list of tracked accounts"""
         raise NotImplementedError
 
@@ -48,45 +50,77 @@ class MongoDbWrapper(DatabaseWrapper):
 
     def __init__(self) -> None:
         """connect to database using credentials"""
-        username: str = os.getenv("MONGO_USERNAME")
-        password: str = os.getenv("MONGO_PASSWORD")
-        cluster_url: str = os.getenv("MONGO_CLUSTER")
+        logger.info("Connecting to MongoDB")
+        username: str = str(os.getenv("MONGO_USERNAME"))
+        password: str = str(os.getenv("MONGO_PASSWORD"))
+        cluster_url: str = str(os.getenv("MONGO_CLUSTER"))
         assert all((username, password, cluster_url))
         mongo_client_url: str = (
             f"mongodb+srv://{username}:{password}@{cluster_url}/mastodon-meter?retryWrites=true&w=majority"
         )
         mongo_client: MongoClient = MongoClient(mongo_client_url)
+        logger.info("Connected to MongoDB")
 
         self._database = mongo_client["mastodon-meter"]
         self._meterings_collection: Collection = self._database["meterings"]
         self._tracked_accounts_collection: Collection = self._database["tracked-accounts"]
 
-    @abstractmethod
-    def add_tracked_account(self, account_id: str, instance_url: str) -> None:
+    @staticmethod
+    def _find_matching_documents(key: str, value: str, collection_: Collection) -> tp.List[Document]:
+        """
+        finds all elements in the specified collection, which have
+        specified key matching specified value
+        """
+        result: tp.List[Document] = list(collection_.find({key: value}))
+
+        for doc in result:
+            del doc["_id"]
+
+        return result
+
+    @staticmethod
+    def _get_all_items_in_collection(collection_: Collection) -> tp.List[Document]:
+        """get all documents in the provided collection"""
+        result: tp.List[Document] = list(collection_.find())
+
+        for doc in result:
+            del doc["_id"]
+
+        return result
+
+    def add_tracked_account(self, account: Account) -> None:
         """add the provided account into the list of tracked accounts"""
-        raise NotImplementedError
+        collection: Collection = self._tracked_accounts_collection
+        collection.insert_one(asdict(account))
 
-    @abstractmethod
-    def delete_tracked_account(self, account_id: str, instance_url: str) -> None:
+    def delete_tracked_account(self, account_internal_id: str) -> None:
         """delete the provided account from the list of tracked accounts"""
-        raise NotImplementedError
+        collection: Collection = self._tracked_accounts_collection
+        collection.delete_one({"internal_id": account_internal_id})
 
-    @abstractmethod
     def get_tracked_accounts(self) -> tp.Set[Account]:
         """get the list of all tracked accounts"""
-        raise NotImplementedError
+        collection: Collection = self._tracked_accounts_collection
+        tracked_accounts = self._get_all_items_in_collection(collection)
+        return {Account(**data) for data in tracked_accounts}
 
-    @abstractmethod
     def add_meterings(self, meterings: tp.Set[Metering]) -> None:
         """add the provided metering into the database"""
-        raise NotImplementedError
+        collection: Collection = self._meterings_collection
+        metering_documents: tp.List[Document] = [asdict(m) for m in meterings]
+        collection.insert_many(metering_documents)
 
-    @abstractmethod
     def delete_meterings(self, metering_ids: tp.Set[str]) -> None:
         """delete the provided metering from the database"""
-        raise NotImplementedError
+        collection: Collection = self._meterings_collection
+        for metering_id in metering_ids:
+            collection.delete_one({"internal_id": metering_id})
 
-    @abstractmethod
     def get_all_meterings(self, account_internal_id: str) -> tp.List[Metering]:
         """get all meterings for an account from the database"""
-        raise NotImplementedError
+        metering_data: tp.List[Document] = self._find_matching_documents(
+            "parent_account_internal_id",
+            account_internal_id,
+            self._meterings_collection,
+        )
+        return [Metering(**data) for data in metering_data]
