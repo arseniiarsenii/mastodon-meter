06f9853bc61f9b2ae4f2176779265c308c95799a
arseniiarsenii <arseniivelichko2@gmail.com>
2021-08-31 19:45:36 +0300

    refactoring

--- src/mastodon_meter/Plotting.py
+++ src/mastodon_meter/Plotting.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 import os
 import typing as tp
 from datetime import datetime as dt
+from functools import reduce
 
-import PIL
 import matplotlib.pyplot as plt
-from loguru import logger
+from PIL import Image
 
 from .Account import Account
 from .Metering import Metering
@@ -17,39 +19,26 @@ class Plotter:
         pass
 
     @staticmethod
-    @logger.catch
-    def draw_plot(
-        data: tp.Tuple[tp.List[str], tp.List[int]],
-        filename: str,
-        title: tp.Optional[str] = None,
-        subtitle: tp.Optional[str] = None,
-        x_label: tp.Optional[str] = None,
-        y_label: tp.Optional[str] = None,
+    def _draw_generic_plot(
+        data: tp.Tuple[tp.List[str], tp.List[int]], filename: str, title: str, x_label: str, y_label: str
     ) -> str:
         """draw a plot with provided data"""
         plt.plot(*data, marker="o")
-
-        if title is not None:
-            plt.title(title)
-
-        subtitle_: str = subtitle or f"Generated on {dt.utcnow().strftime('%Y.%m.%d %H:%M')} UTC using Mastodon-meter"
-        plt.suptitle(subtitle_)
-
-        if x_label is not None:
-            plt.xlabel(x_label)
-
-        if y_label is not None:
-            plt.ylabel(y_label)
+        plt.title(title)
+        subtitle: str = f"Generated on {dt.utcnow().strftime('%Y.%m.%d %H:%M')} UTC using Mastodon-meter"
+        plt.suptitle(subtitle)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
 
         dir_: str = "output"
         if not os.path.exists(dir_):
             os.mkdir(dir_)
 
-        filename_: str = f"{dir_}/{filename}"
-        plt.savefig(filename_)
-        return filename_
+        filename = f"{dir_}/{filename}"
+        plt.savefig(filename)
+        plt.clf()
+        return filename
 
-    @logger.catch
     def draw_subscribers_plot(self, meterings: tp.List[Metering], account: Account) -> str:
         """plot subscribers"""
         x_data: tp.List[str] = [m.timestamp.strftime("%d.%m") for m in meterings]
@@ -58,9 +47,8 @@ class Plotter:
         x_label: str = "Time"
         y_label: str = "Subscriber count"
         filename: str = f"{account.internal_id}-subscribers-plot-{int(dt.utcnow().timestamp())}.png"
-        return self.draw_plot((x_data, y_data), filename, title=title, x_label=x_label, y_label=y_label)
+        return self._draw_generic_plot((x_data, y_data), filename, title, x_label, y_label)
 
-    @logger.catch
     def draw_statuses_plot(self, meterings: tp.List[Metering], account: Account) -> str:
         """plot status count"""
         x_data: tp.List[str] = [m.timestamp.strftime("%d.%m") for m in meterings]
@@ -69,18 +57,29 @@ class Plotter:
         x_label: str = "Time"
         y_label: str = "Statuses count"
         filename: str = f"{account.internal_id}-statuses-plot-{int(dt.utcnow().timestamp())}.png"
-        return self.draw_plot((x_data, y_data), filename, title=title, x_label=x_label, y_label=y_label)
+        return self._draw_generic_plot((x_data, y_data), filename, title, x_label, y_label)
 
-    @logger.catch
     def draw_common_plot(self, meterings: tp.List[Metering], account: Account) -> str:
         """plot statuses and subscribers on the same image"""
-        subscribers_plot: PIL.Image = PIL.Image.open(self.draw_subscribers_plot(meterings, account))
-        statuses_plot: PIL.Image = PIL.Image.open(self.draw_statuses_plot(meterings, account))
-        w1, h1 = subscribers_plot.size
-        w2, _ = statuses_plot.size
-        resulting_image: PIL.Image = PIL.Image.new("RGB", (w1 + w2, h1))
-        resulting_image.paste(subscribers_plot, (0, 0))
-        resulting_image.paste(statuses_plot, (w2, 0))
+        subscribers_plot: Image = Image.open(self.draw_subscribers_plot(meterings, account))
+        statuses_plot: Image = Image.open(self.draw_statuses_plot(meterings, account))
+        resulting_image: Image = self._unite_images([subscribers_plot, statuses_plot])
         filename: str = f"output/{account.internal_id}-common-plot-{int(dt.utcnow().timestamp())}.png"
         resulting_image.save(filename)
         return filename
+
+    @staticmethod
+    def _unite_images(images: tp.List[Image]) -> Image:
+        """make an image that has all the provided images placed on a horizontal line"""
+
+        def _unite_two_images(image_1: Image, image_2: Image) -> Image:
+            """place_two images next to each other horizontally"""
+            w1, h1 = image_1.size
+            w2, h2 = image_2.size
+            resulting_image: Image = Image.new("RGB", (w1 + w2, max((h1, h2))))
+            resulting_image.paste(image_1, (0, 0))
+            resulting_image.paste(image_2, (w2, 0))
+            return resulting_image
+
+        result: Image = reduce(_unite_two_images, images)
+        return result
