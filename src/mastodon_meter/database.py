5e0b75403f81aa320f699aced0f74fd7b3d0c84d
arseniiarsenii <arseniivelichko2@gmail.com>
2021-08-31 14:23:47 +0300

    removed abstract wrapper for now

--- src/mastodon_meter/database.py
+++ src/mastodon_meter/database.py
@@ -1,6 +1,5 @@
 import os
 import typing as tp
-from abc import ABC, abstractmethod
 from dataclasses import asdict
 
 from loguru import logger
@@ -12,41 +11,6 @@ from .Singleton import SingletonMeta
 from .Types import Document
 
 
-class DatabaseWrapper(ABC):
-    """a common database wrapper interface"""
-
-    @abstractmethod
-    def add_tracked_account(self, account: Account) -> None:
-        """add the provided account into the list of tracked accounts"""
-        raise NotImplementedError
-
-    @abstractmethod
-    def delete_tracked_account(self, account_internal_id: str) -> None:
-        """delete the provided account from the list of tracked accounts"""
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_tracked_accounts(self) -> tp.List[Account]:
-        """get the list of all tracked accounts"""
-        raise NotImplementedError
-
-    @abstractmethod
-    def add_meterings(self, meterings: tp.List[Metering]) -> None:
-        """add the provided metering into the database"""
-        raise NotImplementedError
-
-    @abstractmethod
-    def delete_meterings(self, metering_ids: tp.Set[str]) -> None:
-        """delete the provided metering from the database"""
-        raise NotImplementedError
-
-    @abstractmethod
-    def get_all_meterings(self, account_internal_id: str) -> tp.List[Metering]:
-        """get all meterings for an account from the database"""
-        raise NotImplementedError
-
-
-# not using inheritance because ABC and metaclasses don't work well together
 class MongoDbWrapper(metaclass=SingletonMeta):
     """A database wrapper implementation for MongoDB"""
 
@@ -61,11 +25,12 @@ class MongoDbWrapper(metaclass=SingletonMeta):
             f"mongodb+srv://{username}:{password}@{cluster_url}/mastodon-meter?retryWrites=true&w=majority"
         )
         mongo_client: AsyncIOMotorClient = AsyncIOMotorClient(mongo_client_url)
-        logger.info("Connected to MongoDB")
 
         self._database: AsyncIOMotorDatabase = mongo_client["mastodon-meter"]
         self._meterings_collection: AsyncIOMotorCollection = self._database["meterings"]
         self._tracked_accounts_collection: AsyncIOMotorCollection = self._database["tracked-accounts"]
+
+        logger.info("Connected to MongoDB")
 
     @staticmethod
     async def _find_matching_documents(key: str, value: str, collection_: AsyncIOMotorCollection) -> tp.List[Document]:
